71ba41f3d9f6163fe5aa4eb590a998d06676b8ad
willjicn <63469580+willjicn@users.noreply.github.com>
2021-04-21 15:41:44 -0500

    Update chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -11,68 +11,36 @@ function pageLoaded() {
 }
 
 // Button click
-function sendStatement1() {
+function sendStatement(vusremail, vverbid, vverbDisplay, vobjectId, vdefinitionName, vdescription) {
   //makeid(8);
   var statement = {
     "actor": {
-      "mbox": "mailto:" + lastUserMessage,
+      "mbox": "mailto:" + vusremail,
       "name": uUsername,
       "objectType": "Agent"
     },
     "verb": {
-      "id": "https://w3id.org/xapi/adb/verbs/initiated",
+      "id": vverbid,
       "display": {
-        "en-US": "initiated"
+        "en-US": vverbDisplay
       }
     },
     "object": {
-      "id": "http://learningdojo.net/xapi/simple_button",
+      "id": vobjectId,
       "definition": {
         "name": {
-          "en-US": "chatbot"
+          "en-US": vdefinitionName
         },
         "description": {
-          "en-US": "chatbot"
+          "en-US": vdescription
         }
       },
       "objectType": "Activity"
     }
   }
   ADL.XAPIWrapper.sendStatement(statement);
- 
 }
 
-// question 1
-function sendStatement2() {
-  //makeid(8);
-  var statement = {
-    "actor": {
-      "mbox": "mailto:" + lastUserMessage,
-      "name": uUsername,
-      "objectType": "Agent"
-    },
-    "verb": {
-      "id": "https://w3id.org/xapi/dod-isd/verbs/answered",
-      "display": {
-        "en-US": "answered"
-      }
-    },
-    "object": {
-      "id": "http://adlnet.gov/expapi/activities/question",
-      "definition": {
-        "name": {
-          "en-US": "Question 1"
-        },
-        "description": {
-          "en-US": "chatbot"
-        }
-      },
-      "objectType": "Activity"
-    }
-  }
-  ADL.XAPIWrapper.sendStatement(statement);
- 
-}
 
 //****************************************************************************************
 
@@ -89,7 +57,6 @@ function botloadingmsg() {
   //var chatboxmsg = document.getElementById("chatbox").placeholder = "";
   messages.push("<b>" + botName + ": </b>" + "Hello! <br> What is your name?");
   document.getElementById("chatlog" + 1).innerHTML = messages[messages.length - 1];
-
 }
 
 //gloabl variabls
@@ -98,6 +65,7 @@ var uUserEmail = ""; //user email
 var uUsername = ""; //user name as actor
 var vuser = ""; //user name exlude lable
 var userErrormsg = ""; //error messages
+
 
 //Conditional Statements
 function chatbotResponse() {
@@ -114,19 +82,15 @@ function chatbotResponse() {
     var botmsg1 = "Thanks " + lastUserMessage.substring(0, 50) + "!" + " <br> What is your email address?";
 
     //lastUserMessage.split(" ")[0] &  lastUserMessage.split(" ")[1];
-
     var fullName = lastUserMessage.split(" ")[0] + lastUserMessage.split(" ")[1]; //split the string before and after the first space
     vuser = lastUserMessage.split(" ")[0] + " " + lastUserMessage.split(" ")[1]; //add a space between first and last name for the 3dr message dispaly
-
     letterAndemail(letterfiler, errmsg1, botmsg1, fullName); //Overridden method
     return;
   }
 
   if (messages.length.toString() == '3') { //Validate email address
-
   uUsername = lastUserMessage; //email for statements
   uUsername = messages[messages.length - 2].substring(43, 50); //user name for statements
-
   letterfiler = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; //email validation
 
   //letters only
@@ -139,15 +103,15 @@ function chatbotResponse() {
   } else {
     //clear error message
     document.getElementById("inputerrorMsge").innerHTML = "";
-
     vUsername = lastUserMessage.substring(0, 50);
     //chatbox messages
     botMessage = "Thank you, " + vuser + "!" + "<br> <br> We all have unconscious biases. To act on our values for diversity and inclusion, it is important that we identify and interrupt our biases to avoid making decisions based on them. <br> <br> Let’s review how racial bias often turns up the sourcing process for new hires. This chatbot will help you to assess your own ability to interrupt racial bias and it will provide you with tools and resources, if needed. <br><br> Self assessment: Do you agree with the following statement? <br><br> <i>&quot;I am aware of how racial bias occurs in the sourcing process.&quot;</i> <br><br> &nbsp; &nbsp; 1. Strongly agree <br> &nbsp; &nbsp; 2. Agree <br>&nbsp; &nbsp; 3. Unsure <br> &nbsp; &nbsp; 4. Disagree <br>&nbsp; &nbsp;  5. Strongly disagree <br> <br><i>Enter a number to respond.</i>";;
     //Chatlog messages
     messages.push("&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; <b>" + 'You' + ":</b> " + vUsername);
     messages.push("<b>" + botName + ":</b> " + botMessage);
-    sendStatement1(); //initiated
-    sendStatement2(); //send answered question 1 statement
+
+    //initiated
+    sendStatement(lastUserMessage, "https://w3id.org/xapi/adb/verbs/initiated", "initiated", "http://learningdojo.net/xapi/chatbot_initiated", "chatbot_initiated", "chatbot_initiated");
     return;
   }
 }
@@ -159,6 +123,22 @@ function chatbotResponse() {
 
     //Overridden method
     options(numberFiler, newErrMsg, newbotMessage, 5);
+
+    //qustion 1 full answer
+    if (lastUserMessage == 1){
+      lastUserMessage = "1.Strongly agree";
+    } else if (lastUserMessage == 2){
+      lastUserMessage = "2.Agree";
+    } else if  (lastUserMessage == 3){
+      lastUserMessage = "3.Unsure" ;
+    }else if  (lastUserMessage == 4){
+      lastUserMessage = "4.Disagree" ;
+    }else if  (lastUserMessage == 5){
+      lastUserMessage = "5.Strongly disagree";
+    }
+
+    //send statement after user answer question 1
+    sendStatement(vUsername, "https://w3id.org/xapi/dod-isd/verbs/answered", "answered", "http://adlnet.gov/expapi/activities/question", "Question 1", "Questions 1 answer: " + lastUserMessage);
     return;
   }
 
@@ -168,6 +148,20 @@ function chatbotResponse() {
     var newbotMessage = "Thank you for your support and effort. Our goal is to help hiring professionals be actively aware and responsive to potential racial bias that occurs when sourcing all candidates. Its purpose is to prevent racial bias and provide everyone a fair chance. We support diversity inclusion and equity. <br><br> Enter any comments, questions, or feedback below.";
 
     options(numberFiler, newErrMsg, newbotMessage, 3); //Overridden method
+
+    //get question 2 answer
+    vQuestionTwo = lastUserMessage;
+
+    //qustion 2 full answer
+    if (lastUserMessage == 1) {
+      lastUserMessage = "1.Entry level";
+    } else if (lastUserMessage == 2) {
+      lastUserMessage = "2.Mid-Career/Management";
+    } else if (lastUserMessage == 3) {
+      lastUserMessage = "3.Executive (C-level)";
+    }
+    //send statement after user answer question 2
+    sendStatement(vUsername, "https://w3id.org/xapi/dod-isd/verbs/answered", "answered", "http://adlnet.gov/expapi/activities/question", "Question 2", "Questions 2 answer: " + lastUserMessage);
     return;
   }
 
@@ -182,6 +176,10 @@ function chatbotResponse() {
       //Chatlog messages
       messages.push("<b>&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;" + 'You' + ":</b> " + lastUserMessage);
       messages.push("<b>" + botName + ":</b> " + botMessage);
+
+      //send user comments
+      sendStatement(vUsername, "http://adlnet.gov/expapi/verbs/commented", "commented", "http://adlnet.gov/expapi/activities/commented", " ", "USer Comments: " + lastUserMessage);
+
       return;
     } else {
 
@@ -191,6 +189,7 @@ function chatbotResponse() {
       // add messages to Chatlog
       messages.push("<b>&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;" + 'You' + ":</b> " + lastUserMessage);
       messages.push("<b>" + botName + ":</b> " + botMessage);
+
       return;
     }
   }
@@ -204,13 +203,16 @@ function chatbotResponse() {
 
       return;
     } else {
-
       document.getElementById("inputerrorMsge").innerHTML = ""; //clear error message
       //chatbox messages
       botMessage = "Thank you, bye!";
       //Chatlog messages
       messages.push("<b>&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;" + 'You' + ":</b> " + lastUserMessage);
       messages.push("<b>" + botName + ":</b> " + botMessage);
+
+      //After user type in DONE
+      sendStatement(vUsername, "http://adlnet.gov/expapi/verbs/completed", "completed", "http://adlnet.gov/expapi/activities/completed", "Chatbot", "Chatbot completion");
+
       return;
     }
   }
@@ -294,6 +296,7 @@ function keyPress(e) {
     //runs this function when enter is pressed
     newEntry();
   }
+
   if (key == 38) {
     console.log('hi')
     //document.getElementById("chatbox").value = lastUserMessage;
